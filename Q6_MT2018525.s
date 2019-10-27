94a6ada86411761927e6a47854fab04e7a943f3e
swag04 <57060778+swag04@users.noreply.github.com>
2019-10-27 15:48:12 +0530

    Update and rename Ques6.s to Q6_MT2018525.s

--- Q6_MT2018525.s
+++ Q6_MT2018525.s
@@ -6,10 +6,10 @@ __main  FUNCTION
 	   MOV r1,#0x00000004  ; Number a will be stored here
 	   MOV r2,#0x0000000C  ; Number b will be stored here
 	   
-       CMP r1,r2
-	   BNE loop
-loop   SUBGT r1,r1,r2
-       SUBLT r2,r2,r1
+       CMP r1,r2 ;comparison between a and b
+	   BNE loop ; if(a!=b) go to loop 
+loop   SUBGT r1,r1,r2 ; if(a>b) a=a-b
+       SUBLT r2,r2,r1 ; if(b>a) b=b-a
 	   CMP r1,r2
 	   BNE loop
 	   
