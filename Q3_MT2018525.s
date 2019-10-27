9357f2d0c209f269644982a1fbf442d9c4b1e4da
swag04 <57060778+swag04@users.noreply.github.com>
2019-10-27 15:52:04 +0530

    Update Q3_MT2018525.s

--- Q3_MT2018525.s
+++ Q3_MT2018525.s
@@ -6,9 +6,9 @@ __main  FUNCTION
 	   MOV r1,#0x00000441  ; Number to check even or odd is stored in r1
 	   MOV r2,#0x00000001  ; Value 1 is stored in r2
 	   MOV r4,#0x00000000  ; Result will be stored in r4 , if r4 =1 => Odd number else Even Number
-	   AND r5,r1,r2
-	   CMP r5,r4
-	   MOVNE r4,#0x00000001
+	   AND r5,r1,r2 ; AND operation will result in 1 if it is odd number else it will result in 0 and that is stored in R5
+	   CMP r5,r4 ; Then R5 is compared with R4 whose value is 0 
+	   MOVNE r4,#0x00000001 ; If R5 is not equal to R4 that means given number is odd, so 1 is stored in R4
 	   
 	   
 	   
