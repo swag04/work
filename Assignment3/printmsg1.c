227673d7385559413ff01b65d493b58b9dd33735
swag04 <57060778+swag04@users.noreply.github.com>
2019-11-22 20:10:08 +0530

    Add files via upload

--- Assignment3/printmsg1.c
+++ Assignment3/printmsg1.c
@@ -5,11 +5,11 @@ void printMsg1(const int a)
 {
 	 char Msg[100];
 	 char *ptr;
-	 sprintf(Msg, "\n%f", *((float*)&a));
+	 sprintf(Msg, "  %d", a);
 	 ptr = Msg ;
-   while(*ptr != '\0'){
+   while(*ptr != '\0')
+	 {
       ITM_SendChar(*ptr);
-      
-		 ++ptr;
+      ++ptr;
    }
 }
